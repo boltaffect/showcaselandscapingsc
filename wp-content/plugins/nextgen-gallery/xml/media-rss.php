193ac7f709c310e3db70beaadb81e4875389d73e
Nathaniel Pardington <npardington@gmail.com>
2016-01-14 20:21:27 -0500

    Initial Commit

---
+++ wp-content/plugins/nextgen-gallery/xml/media-rss.php
@@ -0,0 +1,120 @@
+<?xml version='1.0' encoding='UTF-8' standalone='yes'?>
+<rss version='2.0' xmlns:media='http://search.yahoo.com/mrss' xmlns:atom='http://www.w3.org/2005/Atom'>
+	<channel>
+		<generator><![CDATA[NextGEN Gallery [http://alexrabe.boelinger.com]]]></generator>
+		<title>Showcase Landscaping SC</title>
+		<description>Beautiful Landscaping in the Upstate</description>
+		<link><![CDATA[http://showcaselandscapingsc.com]]></link>
+		<atom:link rel='next' href='../wp-content/plugins/nextgen-gallery/xml/media-rss.php?show=10&amp;page=1&amp;mode=last_pictures' />
+		<item>
+			<title><![CDATA[webgallery_9]]></title>
+			<description><![CDATA[]]></description>
+			<link><![CDATA[../wp-content/gallery/showcase/webgallery_9.jpg]]></link>
+			<media:content url='../wp-content/gallery/showcase/webgallery_9.jpg' medium='image' />
+			<media:title><![CDATA[webgallery_9]]></media:title>
+			<media:description><![CDATA[]]></media:description>
+			<media:thumbnail url='../wp-content/gallery/showcase/thumbs/thumbs_webgallery_9.jpg' width='100' height='75' />
+			<media:keywords><![CDATA[]]></media:keywords>
+			<media:copyright><![CDATA[Copyright (c) Showcase Landscaping SC (http://showcaselandscapingsc.com)]]></media:copyright>
+		</item>
+		<item>
+			<title><![CDATA[webgallery_8]]></title>
+			<description><![CDATA[]]></description>
+			<link><![CDATA[../wp-content/gallery/showcase/webgallery_8.jpg]]></link>
+			<media:content url='../wp-content/gallery/showcase/webgallery_8.jpg' medium='image' />
+			<media:title><![CDATA[webgallery_8]]></media:title>
+			<media:description><![CDATA[]]></media:description>
+			<media:thumbnail url='../wp-content/gallery/showcase/thumbs/thumbs_webgallery_8.jpg' width='100' height='75' />
+			<media:keywords><![CDATA[]]></media:keywords>
+			<media:copyright><![CDATA[Copyright (c) Showcase Landscaping SC (http://showcaselandscapingsc.com)]]></media:copyright>
+		</item>
+		<item>
+			<title><![CDATA[webgallery_7]]></title>
+			<description><![CDATA[]]></description>
+			<link><![CDATA[../wp-content/gallery/showcase/webgallery_7.jpg]]></link>
+			<media:content url='../wp-content/gallery/showcase/webgallery_7.jpg' medium='image' />
+			<media:title><![CDATA[webgallery_7]]></media:title>
+			<media:description><![CDATA[]]></media:description>
+			<media:thumbnail url='../wp-content/gallery/showcase/thumbs/thumbs_webgallery_7.jpg' width='100' height='75' />
+			<media:keywords><![CDATA[]]></media:keywords>
+			<media:copyright><![CDATA[Copyright (c) Showcase Landscaping SC (http://showcaselandscapingsc.com)]]></media:copyright>
+		</item>
+		<item>
+			<title><![CDATA[webgallery_6]]></title>
+			<description><![CDATA[]]></description>
+			<link><![CDATA[../wp-content/gallery/showcase/webgallery_6.jpg]]></link>
+			<media:content url='../wp-content/gallery/showcase/webgallery_6.jpg' medium='image' />
+			<media:title><![CDATA[webgallery_6]]></media:title>
+			<media:description><![CDATA[]]></media:description>
+			<media:thumbnail url='../wp-content/gallery/showcase/thumbs/thumbs_webgallery_6.jpg' width='100' height='75' />
+			<media:keywords><![CDATA[]]></media:keywords>
+			<media:copyright><![CDATA[Copyright (c) Showcase Landscaping SC (http://showcaselandscapingsc.com)]]></media:copyright>
+		</item>
+		<item>
+			<title><![CDATA[webgallery_5]]></title>
+			<description><![CDATA[]]></description>
+			<link><![CDATA[../wp-content/gallery/showcase/webgallery_5.jpg]]></link>
+			<media:content url='../wp-content/gallery/showcase/webgallery_5.jpg' medium='image' />
+			<media:title><![CDATA[webgallery_5]]></media:title>
+			<media:description><![CDATA[]]></media:description>
+			<media:thumbnail url='../wp-content/gallery/showcase/thumbs/thumbs_webgallery_5.jpg' width='100' height='75' />
+			<media:keywords><![CDATA[new landscape pictures]]></media:keywords>
+			<media:copyright><![CDATA[Copyright (c) Showcase Landscaping SC (http://showcaselandscapingsc.com)]]></media:copyright>
+		</item>
+		<item>
+			<title><![CDATA[webgallery_4]]></title>
+			<description><![CDATA[]]></description>
+			<link><![CDATA[../wp-content/gallery/showcase/webgallery_4.jpg]]></link>
+			<media:content url='../wp-content/gallery/showcase/webgallery_4.jpg' medium='image' />
+			<media:title><![CDATA[webgallery_4]]></media:title>
+			<media:description><![CDATA[]]></media:description>
+			<media:thumbnail url='../wp-content/gallery/showcase/thumbs/thumbs_webgallery_4.jpg' width='100' height='75' />
+			<media:keywords><![CDATA[new landscape pictures]]></media:keywords>
+			<media:copyright><![CDATA[Copyright (c) Showcase Landscaping SC (http://showcaselandscapingsc.com)]]></media:copyright>
+		</item>
+		<item>
+			<title><![CDATA[webgallery_3]]></title>
+			<description><![CDATA[]]></description>
+			<link><![CDATA[../wp-content/gallery/showcase/webgallery_3.jpg]]></link>
+			<media:content url='../wp-content/gallery/showcase/webgallery_3.jpg' medium='image' />
+			<media:title><![CDATA[webgallery_3]]></media:title>
+			<media:description><![CDATA[]]></media:description>
+			<media:thumbnail url='../wp-content/gallery/showcase/thumbs/thumbs_webgallery_3.jpg' width='100' height='75' />
+			<media:keywords><![CDATA[new landscape pictures]]></media:keywords>
+			<media:copyright><![CDATA[Copyright (c) Showcase Landscaping SC (http://showcaselandscapingsc.com)]]></media:copyright>
+		</item>
+		<item>
+			<title><![CDATA[webgallery_2]]></title>
+			<description><![CDATA[]]></description>
+			<link><![CDATA[../wp-content/gallery/showcase/webgallery_2.jpg]]></link>
+			<media:content url='../wp-content/gallery/showcase/webgallery_2.jpg' medium='image' />
+			<media:title><![CDATA[webgallery_2]]></media:title>
+			<media:description><![CDATA[]]></media:description>
+			<media:thumbnail url='../wp-content/gallery/showcase/thumbs/thumbs_webgallery_2.jpg' width='100' height='75' />
+			<media:keywords><![CDATA[new landscape pictures]]></media:keywords>
+			<media:copyright><![CDATA[Copyright (c) Showcase Landscaping SC (http://showcaselandscapingsc.com)]]></media:copyright>
+		</item>
+		<item>
+			<title><![CDATA[webgallery_10]]></title>
+			<description><![CDATA[]]></description>
+			<link><![CDATA[../wp-content/gallery/showcase/webgallery_10.jpg]]></link>
+			<media:content url='../wp-content/gallery/showcase/webgallery_10.jpg' medium='image' />
+			<media:title><![CDATA[webgallery_10]]></media:title>
+			<media:description><![CDATA[]]></media:description>
+			<media:thumbnail url='../wp-content/gallery/showcase/thumbs/thumbs_webgallery_10.jpg' width='100' height='75' />
+			<media:keywords><![CDATA[]]></media:keywords>
+			<media:copyright><![CDATA[Copyright (c) Showcase Landscaping SC (http://showcaselandscapingsc.com)]]></media:copyright>
+		</item>
+		<item>
+			<title><![CDATA[webgallery_1]]></title>
+			<description><![CDATA[]]></description>
+			<link><![CDATA[../wp-content/gallery/showcase/webgallery_1.jpg]]></link>
+			<media:content url='../wp-content/gallery/showcase/webgallery_1.jpg' medium='image' />
+			<media:title><![CDATA[webgallery_1]]></media:title>
+			<media:description><![CDATA[]]></media:description>
+			<media:thumbnail url='../wp-content/gallery/showcase/thumbs/thumbs_webgallery_1.jpg' width='100' height='75' />
+			<media:keywords><![CDATA[new landscape pictures]]></media:keywords>
+			<media:copyright><![CDATA[Copyright (c) Showcase Landscaping SC (http://showcaselandscapingsc.com)]]></media:copyright>
+		</item>
+	</channel>
+</rss>
